44ac1704020ec156b3319357de747c0839edafe1
newush <ushneway@gmail.com>
2020-12-31 15:38:35 -0500

    feat: modify index.hbs

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ const port = 3000
 
 const mongoose = require('mongoose')
 const db = mongoose.connection
-mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
+
+const exphbs = require('express-handlebars');
+
+const Record = require('./models/Record')
+mongoose.connect('mongodb://localhost/expense-tracker', { useNewUrlParser: true, useUnifiedTopology: true })
 db.on('error', () => {
   console.log('mongodb error!')
 })
@@ -12,13 +16,16 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
-const exphbs = require('express-handlebars');
-
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 app.get('/', (req, res) => {
-  res.render('index')
+  Record.find()
+    .lean()
+    .then(records => {
+      res.render('index', { records })
+    })
+    .catch(error => console.error(error))
 })
 app.get('/edit/:id', (req, res) => {
   res.send(`This is edit page`)
