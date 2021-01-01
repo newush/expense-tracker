6346541db5dc8dd09f802cd51379cfb91e51bfed
newush <ushneway@gmail.com>
2021-01-01 18:42:00 -0500

    refactor: modify home.js

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -9,28 +9,23 @@ router.get('/', (req, res) => {
   Category.find()
     .lean()
     .then(categories => {
+      let records = null
       if (category === undefined || category === 'all') {
-
-        return Record.find()
+        records = Record.find()
           .lean()
           .populate('category')
-          .then(records => {
-            records.forEach(record => { totalAmount += record.amount })
-            res.render('index', { records, categories, totalAmount, category })
-          })
-          .catch(error => console.error(error))
       } else {
-        return Record.find({
+        records = Record.find({
           category: req.query.category
         })
           .lean()
           .populate('category')
-          .then(records => {
-            records.forEach(record => { totalAmount += record.amount })
-            res.render('index', { records, categories, totalAmount, category })
-          })
-          .catch(error => console.error(error))
       }
+      records.then(records => {
+        records.forEach(record => { totalAmount += record.amount })
+        res.render('index', { records, categories, totalAmount, category })
+      })
+        .catch(error => console.error(error))
     })
 })
 module.exports = router
