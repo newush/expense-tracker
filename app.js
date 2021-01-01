a179f1029917f9705e36f3c8629de86ab0fedb4e
newush <ushneway@gmail.com>
2021-01-01 17:55:47 -0500

    feat: modify index page ui

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs', helpers: hbsH
 app.set('view engine', 'hbs')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+app.use(express.static('public'))
 app.use(routes)
 
 app.listen(port, () => {
