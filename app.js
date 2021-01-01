dddfe6fa128077630edb930de113b6b5e78b0ab3
newush <ushneway@gmail.com>
2020-12-31 20:52:22 -0500

    feat: add delete record function

--- app.js
+++ app.js
@@ -78,6 +78,13 @@ app.post('/edit/:id', (req, res) => {
     .then(() => res.redirect('/'))
     .catch(error => console.error(error))
 })
+app.post('/delete/:id', (req, res) => {
+  const id = req.params.id
+  return Record.findById(id)
+    .then(record => record.remove())
+    .then(() => res.redirect('/'))
+    .catch(error => console.error(error))
+})
 app.listen(port, () => {
   console.log(`Express is running on http://localhost:${port}`)
 })
