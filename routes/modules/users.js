c758eb9222723a217218b0d675a83358c8aaa83e
newush <ushneway@gmail.com>
2021-01-19 15:40:36 -0500

    feat: add user login page

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -3,4 +3,7 @@ const router = express.Router()
 router.get('/login', (req, res) => {
   res.render('login')
 })
+router.get('/register', (req, res) => {
+  res.render('register')
+})
 module.exports = router
