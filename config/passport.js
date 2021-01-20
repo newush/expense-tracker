92fe32f390368c6464e14d539f00d285fa937618
newush <ushneway@gmail.com>
2021-01-20 00:05:39 -0500

    feat: add user authentication in home and records routers

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,6 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
+const User = require('../models/User')
 module.exports = app => {
   app.use(passport.initialize())
   app.use(passport.session())
