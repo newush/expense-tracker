e7ac4ddec21ac946ce63ffc011d9932b35f5d860
newush <ushneway@gmail.com>
2021-01-19 22:23:51 -0500

    feat: add express-session

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const session = require('express-session')
 const app = express()
 const port = process.env.PORT || 3000
 
@@ -16,6 +17,11 @@ app.set('view engine', 'hbs')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(express.static('public'))
+app.use(session({
+  secret: 'MySecret',
+  resave: false,
+  saveUninitialized: true
+}))
 app.use(routes)
 app.use((req, res) => {
   res.render('error')
