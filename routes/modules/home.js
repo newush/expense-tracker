f20bda67b3bd4566ea37b29b0916d94a4b3dfcf2
newush <ushneway@gmail.com>
2021-01-21 00:40:08 -0500

    feat: add month sorting feature

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -6,26 +6,30 @@ const Record = require('../../models/Record')
 router.get('/', (req, res) => {
   const userId = req.user._id
   let totalAmount = 0
-  const category = req.query.category
+  const category = req.query.category || 'all'
+  const month = req.query.month || 'all'
   Category.find()
     .lean()
     .then(categories => {
       let records = null
-      if (category === undefined || category === 'all') {
+      if (category === 'all') {
         records = Record.find({ userId })
           .lean()
           .populate('category')
       } else {
         records = Record.find({
           userId,
-          category: req.query.category
+          category
         })
           .lean()
           .populate('category')
       }
       records.then(records => {
+        if (month !== 'all') {
+          records = records.filter((record) => record.date.getMonth() === month - 1)
+        }
         records.forEach(record => { totalAmount += record.amount })
-        res.render('index', { records, categories, totalAmount, category })
+        res.render('index', { records, categories, totalAmount, category, month })
       })
         .catch(error => {
           res.render('error')
